71321c3d22c178fe0b7ff764936ddde0130fc7e7
Константин <tur-2002@mail.ru>
2025-03-24 01:26:26 +0700

    Project setup

---
+++ src/index.tsx
@@ -0,0 +1,15 @@
+import {createRoot} from "react-dom/client";
+
+
+const root = document.getElementById('root')
+
+if(!root) {
+    throw new Error('root not found')
+}
+
+const container = createRoot(root)
+
+
+container.render(
+    <>1</>
+)
